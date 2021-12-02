ffa952ffe87bd0204de475a9d9ce6b2e7c22981e
Mark Root-Wiley <info@mrwweb.com>
2021-12-02 11:41:00 -0800

    rebuild all assets after rebase onto font size selector feature

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '546e1d477ca1a0f5c89c411544792991');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd2c11723a7045cbfa2529d119d370023');
