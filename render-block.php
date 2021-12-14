08756414ff9444e57c5b6108d40d825148f9ad50
Mark Root-Wiley <info@mrwweb.com>
2021-12-14 07:42:12 -0800

    add early return if no heading or content. move strip_heading function to utils.php

--- render-block.php
+++ render-block.php
@@ -8,12 +8,19 @@ require( 'utils.php' );
  */
 function render_accordion( $atts, $content ) {
 
-	$accordion_id = isset($atts['anchor']) && !empty($atts['anchor']) ? $atts['anchor'] : $atts['accordionId'];
+	$heading_text = $atts['headingText'] ?? '';
+	$heading_level = $atts['level'] ?? 2;
+	$content = strip_heading( $heading_level, $heading_text, $content );
+
+	if( empty( $heading_text ) && empty( $content ) ) {
+		return;
+	}
+
+	$has_custom_anchor = isset( $atts['anchor'] ) && ! empty( $atts['anchor'] );
+	$accordion_id = $has_custom_anchor ? $atts['anchor'] : $atts['accordionId'];
 	$align = isset($atts['align']) ? ' align' . $atts['align'] : '';
 	$primary_color = $atts['primaryColor'] ?? false;
-	$heading_level = $atts['level'] ?? 2;
 	$heading_tag = 'h' . $heading_level;
-	$heading_text = $atts['headingText'] ?? '';
 	$heading_classes = get_accordion_heading_classes(
 		$atts['headingFontSize'] ?? false,
 		$atts['primaryColor'] ?? false,
@@ -26,14 +33,10 @@ function render_accordion( $atts, $content ) {
 	}
 	$icon_svg = file_get_contents( plugin_dir_path(__FILE__) .'img/' . $icon . '.svg' );
 
-	/* Due to current "Hybrid Block" strategy, I now need to filter out the first block which is the heading */
-	$search_string = "<h${heading_level}>${heading_text}</h${heading_level}>";
-	$content = str_replace( $search_string, '', html_entity_decode($content) );
-
 	ob_start();
 
-	if( ! isset( $atts['anchor'] ) ) {
-		echo '<!-- ⚠ If you want to make an anchor link to the following accordion, you must set a custom anchor ID in the Advanced > "HTML anchor" sidebar setting of the accordion. The random ID is _not_ stable and may change each time the page is saved again. -->';
+	if( ! $has_custom_anchor ) {
+		echo __( '<!-- ⚠ To link to the next accordion section, set a custom ID in the block sidebar settings ("Advanced" > "HTML anchor"). The random ID is NOT stable and may change the next time this page is saved. -->', 'mrw-accordion' );
 	}
 	?>
 	<div
--- utils.php
+++ utils.php
@@ -1,6 +1,14 @@
 <?php
 namespace MRW\Accordion;
 
+/**
+ * generate the full string for all classes that may appear on the accordion
+ *
+ * @param boolean $font_size whether there is a custom font size
+ * @param boolean $bg_color whether there is a custom background color
+ * @param boolean $text_color whether there is a custom text color
+ * @return string full space-separated list of classes for accordion heading
+ */
 function get_accordion_heading_classes( $font_size = false, $bg_color = false, $text_color = false ) {
 
 	$classes = 'mrw-accordion__heading';
@@ -29,4 +37,18 @@ function get_accordion_heading_classes( $font_size = false, $bg_color = false, $
 	}
 
 	return $classes;
+}
+
+/**
+ * Remove a heading and its content from an HTML string
+ *
+ * @param int $level level of heading
+ * @param string $heading heading content
+ * @param string $content content to replace heading in
+ * @return string $content with heading removed
+ */
+function strip_heading( $level = 2, $heading, $content ) {
+	/* Due to current "Hybrid Block" strategy, I now need to filter out the first block which is the heading */
+	$search_string = "<h${level}>${heading}</h${level}>";
+	return trim( str_replace( $search_string, '', html_entity_decode( $content ) ) );
 }
