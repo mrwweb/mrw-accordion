245a86795fedf0bffc280d74fde754d103cee72a
Mark Root-Wiley <info@mrwweb.com>
2024-04-03 16:54:52 -0700

    add explicit width to accordion icon setting. fixes #41

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ba037db8e277f353a4de');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4b58443a5e9e2f05ce78');
