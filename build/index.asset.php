a1432592c0cbaa0eb5cf0ca139af1ea2dd177251
Mark Root-Wiley <info@mrwweb.com>
2024-04-12 16:02:37 -0700

    fix dumb mistake with wrong custom property name for padding. explicitly block text-wrap: balance for heading which appears able to break flexbox (at least in Firefox)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e334ee6afac54800cdc');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0fbe71ef726aa7d136ed');
